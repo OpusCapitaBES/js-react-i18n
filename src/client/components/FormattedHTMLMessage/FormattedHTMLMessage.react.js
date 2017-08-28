f0dee48663c1ec5615fb30e350d34f500ef05d90
Alexey Sergeev <sab@scand.com>
2017-08-28 22:30:43 +0300

    Removing lodash usage

--- src/client/components/FormattedHTMLMessage/FormattedHTMLMessage.react.js
+++ src/client/components/FormattedHTMLMessage/FormattedHTMLMessage.react.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import lodash from 'lodash';
 
 export default class FormattedHTMLMessage extends React.Component {
-
   static propTypes = {
     message: React.PropTypes.string,
   };
@@ -12,9 +10,9 @@ export default class FormattedHTMLMessage extends React.Component {
   };
 
   render() {
-    let message = null;
-    if (this.props.message) {
-      message = this.context.i18n.getMessage(this.props.message, lodash.omit(this.props, 'message'));
+    let { message, ...messageArguments } = this.props;
+    if (message) {
+      message = this.context.i18n.getMessage(message, messageArguments);
     }
 
     return (<span dangerouslySetInnerHTML={ { __html: message } } />);
